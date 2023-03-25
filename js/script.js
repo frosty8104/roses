d191de5060e2174348eaba69232a3834ea48978f
laura <lg8104@gmail.com>
2023-03-25 15:19:25 -0600

    Creating Ryan's Roses repo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Change the background color of the footer to #d7ddf3
+//var footer = document.querySelector(".contact");
+//footer.style.backgroundColor = "#d7ddf3";
+//var tagline = document.querySelector(".tagline");
+//tagline.innerText = "Let us help you say it!";
+//var address = document.querySelector("address");
+//address.style.lineHeight = "1.5em";
+//address.style.fontSize = "1.75em";
+//var everyDay = document.querySelector("h3");
+//everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+var hours = document.querySelector(".contact p");
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
